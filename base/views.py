03ef3166e684ffb52fadcfe440b2c8d777a33e0b
karamfm <www.karam777krm@gmail.com>
2021-11-16 19:17:50 +0300

    set up room in 2:5/7

--- base/views.py
+++ base/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . models import Room
+from .forms import RoomForm
 # Create your views here.
+
+
+def home(request):
+    rooms = Room.objects.all()
+    contxt = {
+        "rooms": rooms
+    }
+    return render(request, "base/home.html", contxt)
+
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    contxt = {
+        "room": room
+    }
+    return render(request, "base/room.html", contxt)
+
+
+def createroom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    contxt = {
+        "form": form
+    }
+    return render(request, 'base/room_form.html', contxt)
+
+
+def updateroom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    contxt = {
+        "form": form
+    }
+    return render(request, 'base/room_form.html', contxt)
+
+
+def deleteroom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    contxt = {
+        "obj": room
+    }
+    return render(request, 'base/delete.html', contxt)
