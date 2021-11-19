15b090aa3cab289ceaa928e6d5ad6e150ee99fe3
karamfm <www.karam777krm@gmail.com>
2021-11-19 14:22:09 +0300

    make mobile responsive 5:53/7

--- base/urls.py
+++ base/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path('delete_room/<str:pk>/', views.deleteroom, name="delete_room"),
     path('delete_message/<str:pk>/', views.deletemessage, name="delete_message"),
     path('update_user/', views.updateuser, name="update_user"),
+    path('topics/', views.topicsPage, name="topics"),
+    path('activity/', views.activityPage, name="activity"),
 ]
--- base/views.py
+++ base/views.py
@@ -65,7 +65,7 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q)
     )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
 
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
@@ -73,7 +73,7 @@ def home(request):
         "rooms": rooms,
         "topics": topics,
         "room_count": room_count,
-        "room_messages": room_messages
+        "room_messages": room_messages,
     }
     return render(request, "base/home.html", contxt)
 
@@ -202,3 +202,20 @@ def updateuser(request):
         'form': form
     }
     return render(request, 'base/update_user.html', contxt)
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    contxt = {
+        'topics': topics
+    }
+    return render(request, 'base/topics.html', contxt)
+
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    contxt = {
+        "room_messages": room_messages
+    }
+    return render(request, 'base/activity.html', contxt)
