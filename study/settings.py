92c27bd2608c326de6522f8aebf6e0519ed709b8
karamfarhan <www.karam777krm@gmail.com>
2022-05-16 19:57:43 +0300

    updated last

--- study/settings.py
+++ study/settings.py
@@ -55,11 +55,10 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = 'base.User'
 
 MIDDLEWARE = [
+    
     'django.middleware.security.SecurityMiddleware',
-
-    "corsheaders.middleware.CorsMiddleware",
-
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
